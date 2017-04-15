1b3c5a031378a4bf46fa8f734efc4bf63d5fd8f2
shonenada <shonenada@gmail.com>
2017-04-15 22:01:00 +0800

    chore(base): framework of ts, react and webpack

---
+++ src/App.tsx
@@ -0,0 +1,16 @@
+import * as React from 'react';
+import './App.css';
+
+class App extends React.Component<any, any> {
+  render() {
+    return (
+      <div className="App">
+        <div className="App-header">
+          <h2>Welcome to Niffler</h2>
+        </div>
+      </div>
+    );
+  }
+}
+
+export default App;
